8685250e0905b0f6e7595886e1cf534f3720ad87
Hardik052 <bhardwajhardik012@gmail.com>
2023-07-12 23:27:32 -0500

    changed the per limit of the pagination

--- app/controllers/data_collection_controller.rb
+++ app/controllers/data_collection_controller.rb
@@ -1,7 +1,7 @@
 class DataCollectionController < ApplicationController
   def index
     if params[:search]
-      @data_collection = Quote.where("content LIKE ?", "%#{params[:search]}%").page(params[:page]).per(2)
+      @data_collection = Quote.where("content LIKE ?", "%#{params[:search]}%").page(params[:page]).per(3)
     else
       @data_collection = Quote.page(params[:page]).per(2)
     end
